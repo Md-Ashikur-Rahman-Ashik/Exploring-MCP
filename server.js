4f19ad6c71453556fea2db208a868744d0c873a9
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2026-03-22 20:49:34 +0600

    Refactor import statement for Google APIs in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import {McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
 import {StdioServerTransport} from "@modelcontextprotocol/sdk/server/stdio.js";
 import dotenv from "dotenv";
-import google from "googleapis";
+import {google} from "googleapis";
 import {z} from "zod";
 
 dotenv.config();
